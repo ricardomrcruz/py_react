1620ff937c6bbf155eb4ed37dbea37535034f716
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-05-21 00:57:09 +0200

    backend route handlers

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = map(lambda x: x.to_json(), contacts)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
