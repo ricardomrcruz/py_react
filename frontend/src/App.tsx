428f3c384797172e60c488aa351bdfd91ee00646
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-05-24 00:57:40 +0200

    front fetch success

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [contacts, setContacts] = useState([]);
 
-  return 
-  
-  <>
+  useEffect(() => {
+    fetchContacts();
+  }, []);
 
-  </>;
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  };
+  return <></>;
 }
 
 export default App;
