599ae4d7addfa656294fd404d585be103a12f6e8
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-12 16:23:56 +0200

    route test

--- fastapi/app.py
+++ fastapi/app.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+students = {1: {"name": "John", "age": 17, "class": "Year 12"}}
 
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+
+@app.get("/get-student/{student-id}")
+def get_student(
+    student_id: int = Path(
+        None, description="The ID of the student you want to view", gt=0, lt=3
+    )
+):
+    return students[student_id]
