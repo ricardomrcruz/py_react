10eacec7a07295001246afb64ad98d2eb1294c13
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-17 00:41:09 +0200

    models

--- app/main.py
+++ app/main.py
@@ -3,3 +3,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"name": "raw"}
---
+++ app/markets/schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class Product(BaseModel):
+    id: int
+    title: str 
+    price: int
+    img: str 
+    description: str 
+    url: str
+
+    class Config:
+        validate_assignement = True
+
+
+class Category(BaseModel):
+    id: int
+    name: str
+
+
+class ProductByCategory(BaseModel):
+    id: int
+    product_id: Product.id
+    category_id: Category.id
+
+
+
+
+
