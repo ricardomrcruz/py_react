050cceb8bc085b79b5f6b169b8deceb584b7335d
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-18 00:02:21 +0200

    structure and models, examples

---
+++ root/app/markets/examples.py
@@ -0,0 +1,23 @@
+
+product={ 
+    "id":"1",
+    "title":"PS5 Spiderman Bundle Vr",
+    "price":"499,99",
+    "img":"www.imgipsum.com", 
+    "description":"some 5ps5 bundle",
+    "url":"www.vlfkxhjgklsfml.fr"
+    }
+
+category={ 
+    "id":"1",
+    "name":"Sony Playstation 5 Pro",
+    }
+ProductByCategory={ 
+    "product_id":"1",
+    "category_id":"1",
+    }
+
+
+
+
+
--- root/app/markets/schemas.py
+++ root/app/markets/schemas.py
@@ -27,3 +27,5 @@ class ProductByCategory(BaseModel):
 
 
 
+
+
