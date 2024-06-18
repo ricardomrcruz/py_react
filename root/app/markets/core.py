596d8da98c1a0a7e99348bc8b309fb437f5fb9e9
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-19 01:20:30 +0200

    core1 success

--- root/app/markets/core.py
+++ root/app/markets/core.py
@@ -2,19 +2,18 @@ from playwright.async_api import async_playwright
 import asyncio
 
 
-query = "playstation+5+console"
-
-
 async def main():
+
     browsers = ["chromium", "firefox", "webkit"]
     async with async_playwright() as p:
         for browser_type in browsers:
+
             browser = await p[browser_type].launch()
             page = await browser.new_page()
-            await page.goto("https://www.amazon.fr/s?k={query}")
-            page.screenshot(path=f"py_{browser_type}.png", full_page=True)
+            query = "playstation+5+console"
+            await page.goto(f"https://www.amazon.fr/s?k={query}")
+            await page.screenshot(path=f"py_{browser_type}.png", full_page=True)
             await page.wait_for_timeout(1000)
-            await browser.close()
 
             all_products = await page.query_selector_all(
                 ".s-card-container > .a-spacing-base"
@@ -22,14 +21,18 @@ async def main():
             data = []
             for product in all_products:
                 result = dict()
-                title_el = await product.query_selector(".a-size-mini")
+                title_el = await product.query_selector(".a-size-base-plus")
                 result["title"] = await title_el.inner_text() if title_el else None
                 price_el = await product.query_selector(".a-price")
                 result["price"] = await price_el.inner_text() if price_el else None
                 rating_el = await product.query_selector(".a-icon-alt")
                 result["rating"] = await rating_el.inner_text() if rating_el else None
-                img_el = await product.query_selector(".a-img")
-                result["img"] = await img_el.inner_text() if img_el else None
+                img_el = await product.query_selector(".s-image-optimized-rendering")
+                result["img"] = await img_el.get_attribute("src") if img_el else None
+                data.append(result)
+            print(data)
+            await browser.close()
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
