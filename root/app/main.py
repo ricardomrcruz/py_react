2e4e218a906dcd10a810ca0571f8cbeed73cec7d
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-24 00:06:55 +0200

    main upd

--- root/app/main.py
+++ root/app/main.py
@@ -4,9 +4,29 @@ from pydantic import BaseModel
 
 # from app.db.schemas import User, Product, Category, ProductByCategory, Market, UserResearch, UserCreate, User
 
-app = FastAPI()
+app = FastAPI(title="API", description="api test", docs_url="/")
 
 
-@app.get("/")
-def index():
-    return {"name": "First Data"}
+@app.get("/products")
+async def get_all_products():
+    pass
+
+
+@app.post("/products")
+async def create_product():
+    pass
+
+
+@app.get("/product/{product_id}")
+async def get_product_by_id(product_id):
+    pass
+
+
+@app.patch("/product/{product_id}")
+async def update_product_by_id(product_id):
+    pass
+
+
+@app.delete("/product/{product_id}")
+async def delete_product_by_id():
+    pass
