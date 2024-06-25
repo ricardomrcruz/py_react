00c9cf29109746f71f9cdf3af3d2e5e595799f31
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-06-25 19:50:44 +0200

    create db fix | successful db creation

--- root/app/db/create_db.py
+++ root/app/db/create_db.py
@@ -1,17 +1,18 @@
-from .database import Base, engine
+from app.db.database import Base, engine
+from app.db.models import Product, Category, User
 import asyncio
 
 
 async def create_db():
     async with engine.begin() as conn:
-        from models import Product
+       
 
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
     await engine.dispose()
 
-# asyncio.run(create_db())
+asyncio.run(create_db())
 
 
 
