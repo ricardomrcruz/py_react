45f4c3e5ccfb2489fb7b7d786fb472917d105aec
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-07-08 03:06:38 +0200

    jinja2 setup done

--- root/app/main.py
+++ root/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+import os
+from fastapi import FastAPI, Request, HTTPException
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from typing import Optional, List, Annotated
 from http import HTTPStatus
@@ -6,23 +7,26 @@ from app.db.repositories import CRUD
 from app.db.database import engine
 from app.db.models import Product as DBProduct
 from app.db.create_db import create_db
-from app.db.schemas import (
-    User,
-    Product,
-    CreateProductModel,
-    Category,
-    ProductByCategory,
-    Market,
-    UserResearch,
-    UserCreate,
-    User,
-)
-import uuid
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from app.router.api_v1 import endpoints as api_endpoints
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI(title="API", description="api test", docs_url="/")
 
-session = async_sessionmaker(bind=engine, expire_on_commit=False)
-
 db = CRUD()
+
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+# API routes
+app.include_router(api_endpoints.router, prefix="/api/v1", tags=["api"])
+
+
+@app.get("/index")
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
