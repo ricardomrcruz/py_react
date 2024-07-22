cf04f67a3a0dc2f62e4ece64ee87450c03312e5e
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-07-22 16:25:55 +0200

    post register user

--- root/app/auth.py
+++ root/app/auth.py
@@ -11,49 +11,49 @@ from app.db.models import User
 from app.db.database import engine
 
 
+class AuthHandler:
 
-class AuthHandler():
-    
     # token decoding for signature validation
     security = HTTPBearer()
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     ALGORITHM = "HS256"
-    SECRET_KEY="d81616e2761cb6e4ba619e79e5f85df0f456659be1dad5a5427250d131e1f30f" 
+    SECRET_KEY = "d81616e2761cb6e4ba619e79e5f85df0f456659be1dad5a5427250d131e1f30f"
     ACCESS_TOKEN_EXPIRE_MINUTES = 60
-    
-    def decode_token(self,token):
+
+    def decode_token(self, token):
         try:
             payload = jwt.decode(token, self.SECRET_KEY, algorithms=self.ALGORITHM)
-            return payload['sub']
+            return payload["sub"]
         except jwt.ExpiredSignatureError:
             raise RequiresLoginException()
         except jwt.JWTError as e:
             raise RequiresLoginException()
         except Exception as e:
             raise RequiresLoginException()
-        
-    def auth_wrapper(self, auth: HTTPAuthorizationCredentials =Security(security)):
+
+    def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
         return self.decode_token(auth.credentials)
-    
+
     #  jwt token creation
-    def create_access_token(self, 
-        subject: Union[str, Any], expires_delta: timedelta = None
-    )->str:
+    def create_access_token(
+        self, subject: Union[str, Any], expires_delta: timedelta = None
+    ) -> str:
         if expires_delta:
             expire = datetime.now(datetime.UTC) + expires_delta
         else:
             expire = datetime.now(datetime.UTC) + timedelta(
-                minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
-        to_encode = {"exp": expire, "sub":str(subject)}
+                minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES
+            )
+        to_encode = {"exp": expire, "sub": str(subject)}
         encoded_jwt = jwt.encode(to_encode, self.SECRET_KEY, algotithm=self.ALGORITHM)
         return encoded_jwt
-    
+
     def get_hash_password(self, plain_password):
         return self.pwd_context.hash(plain_password)
-    
+
     def verify_password(self, plain_password, hash_password):
         return self.pwd_context.verify(plain_password, hash_password)
-    
+
     # email and password verification
     async def authenticate_user(self, email, password):
         try:
@@ -61,23 +61,15 @@ class AuthHandler():
                 query = select(User).where(User.email == email)
                 result = await session.execute(query)
                 user = result.scalar_one_or_none()
-                
+
             if user and self.verify_password(password, user.hashed_password):
                 return user
             else:
                 raise RequiresLoginException()
-        except:
+        except Exception as e:
             print(f"Authentication error: {str(e)}")
             raise RequiresLoginException()
-         
-    
-    
-    
-        
-    
-        
+
 
 class RequiresLoginException(Exception):
     pass
-    
-    
--- root/app/main.py
+++ root/app/main.py
@@ -12,37 +12,90 @@ from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.router.api_v1 import endpoints as api_endpoints
-from fastapi.responses import HTMLResponse
-from app.router import htmx_components 
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
+from app.router import htmx_components
+from app.auth import AuthHandler, RequiresLoginException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.models import User
+from app.db.database import engine
 
 
 app = FastAPI(title="API", description="api test", docs_url="/docs")
 
 db = CRUD()
 
-
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 templates = Jinja2Templates(directory="app/templates")
+auth_handler = AuthHandler()
+
+
+# redirection from exception to index
+@app.exception_handler(RequiresLoginException)
+async def exception_handler(request: Request, exc: RequiresLoginException) -> Response:
+    return RedirectResponse(url="/")
+
+
+@app.middleware("http")
+async def create_auth_header(request:Request, call_next):
+    '''
+    Checks if there are cookies set for authorization. If so, contruct 
+    the authorization header and modify the request (unless the header 
+    already exists!)
+    '''
+    if ("Authorization" not in request.headers
+        and "Authorization" in request.cookies):
+        access_token = request.cookies["Authorization"]
+        
+        request.headers.__dict__["_list"].append(
+            (
+                "authorization".encode(),
+                f"Bearer{access_token}".encode(),
+                                       
+            )
+        )
+    elif ("Authorization" not in request.headers
+        and "Authorization" not in request.cookies):
+        request.headers.__dict__["_list"].append(
+            (
+                "authorization".encode(),
+                f"Bearer 1234".encode(),
+                                       
+            )
+        )
+        
+    response = await call_next(request)
+    return response
+        
+
 
 # API routes
 app.include_router(api_endpoints.router, prefix="/api/v1", tags=["api"])
 app.include_router(htmx_components.router)
-
+    
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse({"request": request}, name="index.html")
 
+
 @app.get("/dashboard", response_class=HTMLResponse)
 async def dashboard(request: Request):
     return templates.TemplateResponse({"request": request}, name="dashboard.html")
+
 
 @app.get("/signin", response_class=HTMLResponse)
 async def dashboard(request: Request):
     return templates.TemplateResponse({"request": request}, name="login.html")
 
-
-
-
-
+# @app.post("/register/", response_class=HTMLResponse)
+# async def register(request:Request, email: str = Form(...), password: str=Form(...)):
+#     try:
+#         async with AsyncSession(engine) as session:
+#         query = insert(User).where(User.email == email)
+#         result = await session.execute(query)
+#         user = result.scalar_one_or_none()
+#     catch:
+        
+   
