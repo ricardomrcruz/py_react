3719707c1b509dba049e8f592ac1a13624592907
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-07-30 16:27:32 +0200

    dockerfiles

--- root/scraper/app/scraper.py
+++ root/scraper/app/scraper.py
@@ -2,15 +2,6 @@ import subprocess
 import json
 
 def scrape_amazon(query: str):
-    try:
-        result = subprocess.run(['python', 'scraper/app/core1.py', query], capture_output=True, text=True,  check=True)
-        return json.loads(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Subprocess error: {e}")
-        print(f"Stdout: {e.stdout}")
-        print(f"Stderr: {e.stderr}")
-        raise
-    except json.JSONDecodeError as e:
-        print(f"JSON decode error: {e}")
-        print(f"Raw output: {result.stdout}")
-        raise
+    result = subprocess.run(['python', 'scraper/app/core1.py', query], capture_output=True, text=True,  check=True)
+    return json.loads(result.stdout)
+    
